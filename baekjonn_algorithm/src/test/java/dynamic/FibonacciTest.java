4f28936f024233074952ec7911233409ba00129d
handr95 <handr95@naver.com>
2019-06-27 22:35:11 +0900

    * algorithm study

---
+++ baekjonn_algorithm/src/test/java/dynamic/FibonacciTest.java
@@ -0,0 +1,12 @@
+package dynamic;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+    @Test
+    public void fibonacci_test() {
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.solution();
+    }
+}
