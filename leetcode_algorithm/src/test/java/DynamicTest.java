644dcd2f067a9a40daeb7462ae832ec05116cbf4
handr95 <handr95@naver.com>
2019-07-01 09:54:10 +0900

    * leetcode algorithm study : Longest Palindromic Substring algorithm

--- leetcode_algorithm/src/test/java/DynamicTest.java
+++ leetcode_algorithm/src/test/java/DynamicTest.java
@@ -1,9 +1,19 @@
-import dynamic.RegularExpressionMatchiing;
 import org.junit.Test;
+
+import dynamic.LongestPalindrome;
+import dynamic.RegularExpressionMatchiing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DynamicTest {
+
+    @Test
+    public void longest_palindrome() {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        assertThat(longestPalindrome.longestPalindrome("babad")).isEqualTo("bab");
+        assertThat(longestPalindrome.longestPalindrome("cbbd")).isEqualTo("bb");
+        assertThat(longestPalindrome.longestPalindrome("ccc")).isEqualTo("ccc");
+    }
 
     @Test
     public void regular_expression_matching_test() {
