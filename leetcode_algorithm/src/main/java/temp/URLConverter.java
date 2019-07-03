f703ff1c79acf56280849a675c416b468e142157
handr95 <handr95@naver.com>
2019-07-03 23:32:13 +0900

    * algorithm study

---
+++ leetcode_algorithm/src/main/java/temp/URLConverter.java
@@ -0,0 +1,25 @@
+package temp;
+
+/**
+ * 배열과 문자열
+ *
+ * 1.3 URL화
+ * 문자열에 들어 있는 모든 공백을 '%20'바꿔 주는 메서드를 작성하라.
+ * 최종적으로 모든 문자를 다 담을 수 있을 만큼 충분한 공간이 이미 확보되어 있으며 문자열의 최종 길이가 함께 주어진다고 가정해도된다.
+ * (자바로 구현한다면 배열 안에서 작업할 수 있도록 문자 배열을 이용하길 바란다.)
+ */
+public class URLConverter {
+    public String urlConverter(String url) {
+        char[] chars = url.toCharArray();
+        String convertURL = "";
+        for(int i = 0; i < chars.length; i ++){
+            if(chars[i] == ' ') {
+                convertURL += "%20";
+            } else {
+                convertURL += chars[i];
+            }
+        }
+
+        return convertURL;
+    }
+}
--- leetcode_algorithm/src/test/java/TempTest.java
+++ leetcode_algorithm/src/test/java/TempTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+import temp.CircularPermutationCheck;
 import temp.PermutationCheck;
+import temp.URLConverter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,5 +10,17 @@ public class TempTest {
     public void permutaion_check() {
         PermutationCheck permutationCheck = new PermutationCheck();
         assertThat(permutationCheck.permutation_check("abcde", "edbac")).isTrue();
+    }
+
+    @Test
+    public void url_converter_test() {
+        URLConverter urlConverter = new URLConverter();
+        assertThat(urlConverter.urlConverter("Mr John Smith")).isEqualTo("Mr%20John%20Smith");
+    }
+
+    @Test
+    public void circular_permutation_check_test() {
+        CircularPermutationCheck circularPermutationCheck = new CircularPermutationCheck();
+        assertThat(circularPermutationCheck.circular_permutation_check("Tact Coa")).isTrue();
     }
 }
