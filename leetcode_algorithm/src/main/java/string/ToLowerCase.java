f467609f856fa526f3192705f8692655700b516d
handr95 <handr95@naver.com>
2019-07-06 22:49:22 +0900

    * algorithm study

---
+++ leetcode_algorithm/src/main/java/string/ToLowerCase.java
@@ -0,0 +1,27 @@
+package string;
+
+/**
+ * 709. To Lower Case ( https://leetcode.com/problems/to-lower-case/)
+ *
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: "Hello"
+ * Output: "hello"
+ * Example 2:
+ *
+ * Input: "here"
+ * Output: "here"
+ * Example 3:
+ *
+ * Input: "LOVELY"
+ * Output: "lovely"
+ */
+public class ToLowerCase {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
